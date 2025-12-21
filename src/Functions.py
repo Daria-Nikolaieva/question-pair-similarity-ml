8158865716497cefe6df1682dffa3677bc978665
Daria-Nikolaieva <nikolaeva.daria.w@gmail.com>
2025-12-22 01:37:08 +0200

    result

---
+++ src/Functions.py
@@ -0,0 +1,109 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords, wordnet
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial.distance import cityblock, euclidean
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from rapidfuzz import fuzz
+
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
+
+def common_words(q1, q2):
+    return len(set(q1) & set(q2))
+
+def jaccard(q1, q2):
+    if len(set(q1) | set(q2)) == 0:
+        return 0
+    return len(set(q1) & set(q2)) / len(set(q1) | set(q2))
+
+def ngram_overlap_tokens(t1, t2, n=2):
+    ngrams1 = set(tuple(t1[i:i+n]) for i in range(len(t1)-n+1))
+    ngrams2 = set(tuple(t2[i:i+n]) for i in range(len(t2)-n+1))
+
+    if len(ngrams1 | ngrams2) == 0:
+        return 0
+
+    return len(ngrams1 & ngrams2) / len(ngrams1 | ngrams2)
+
+def get_wordnet_pos(tag):
+    """
+    Преобразует часть речи из nltk.pos_tag в формат wordnet:
+    NN -> n, VB -> v, JJ -> a, RB -> r.
+    """
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN 
+
+def lemma_tokenizer(text):
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(text.lower())
+    # убрать мусор и стоп-слова
+    tokens = [t for t in tokens if t.isalpha() and t not in stop_words]
+    # POS-тэги
+    pos_tags = nltk.pos_tag(tokens)
+    # лемматизация
+    lemmas = [
+        lemmatizer.lemmatize(token, get_wordnet_pos(tag))
+        for token, tag in pos_tags
+    ]
+    return lemmas
+
+def data_processing(df):
+
+    df['q1_tokens'] = df['question1'].apply(lemma_tokenizer)
+    df['q2_tokens'] = df['question2'].apply(lemma_tokenizer)
+    
+    df['q1_clean'] = df['q1_tokens'].apply(lambda x: " ".join(x))
+    df['q2_clean'] = df['q2_tokens'].apply(lambda x: " ".join(x))
+    
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(pd.concat([df["q1_clean"], df["q2_clean"]]))
+    tfidf_q1 = vectorizer.transform(df["q1_clean"])
+    tfidf_q2 = vectorizer.transform(df["q2_clean"])
+    
+    # Добавляем признаки
+    df["common_words"] = df.apply(lambda x: common_words(x['q1_tokens'], x['q2_tokens']), axis=1)
+    df["jaccard"] = df.apply(lambda x: jaccard(x['q1_tokens'], x['q2_tokens']), axis=1)
+    df['lev_ratio'] = df.apply(lambda x: fuzz.ratio(str(x['question1']), str(x['question2'])) / 100, axis=1)
+    df["bigram_overlap"] = df.apply(lambda row: ngram_overlap_tokens(row.q1_tokens, row.q2_tokens, n=2),axis=1)
+    df["trigram_overlap"] = df.apply(lambda row: ngram_overlap_tokens(row.q1_tokens, row.q2_tokens, n=3),axis=1)
+
+    return df
+
+def tfidf_features(df, vectorizer=None):
+    df = data_processing(df)
+    if vectorizer is None:
+        vectorizer = TfidfVectorizer()
+        vectorizer.fit(pd.concat([df["q1_clean"], df["q2_clean"]]))
+
+    # Если передали — только transform
+    tfidf_q1 = vectorizer.transform(df["q1_clean"])
+    tfidf_q2 = vectorizer.transform(df["q2_clean"])
+
+    # distance features
+    df["cosine_sim"] = [cosine_similarity(tfidf_q1[i], tfidf_q2[i])[0][0] 
+                        for i in range(len(df))]
+    df["manhattan"] = [cityblock(tfidf_q1[i].toarray().ravel(),
+                                 tfidf_q2[i].toarray().ravel()) 
+                       for i in range(len(df))]
+    df["euclidean"] = [euclidean(tfidf_q1[i].toarray().ravel(),
+                                 tfidf_q2[i].toarray().ravel()) 
+                       for i in range(len(df))]
+
+    return df, vectorizer, tfidf_q1, tfidf_q2
